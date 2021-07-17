051c5394ff9cefea0dc0568737f66b3c71391e72
Richard <richard.yang.tong@gmail.com>
2021-07-17 16:41:13 -0400

    Added notes.

--- spring-annotation/src/main/java/com/dragontalker/config/MainConfig.java
+++ spring-annotation/src/main/java/com/dragontalker/config/MainConfig.java
@@ -1,8 +1,15 @@
 package com.dragontalker.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.dragontalker.bean.Person;
 
 @Configuration
 public class MainConfig {
 
+	@Bean
+	public Person person() {
+		return new Person("lisi", 20);
+	}
 }
