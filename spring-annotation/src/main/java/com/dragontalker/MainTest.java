dc16b6417345c4f8904078d2301d381081cd838d
Richard <richard.yang.tong@gmail.com>
2021-07-17 16:38:30 -0400

    Added notes.

--- spring-annotation/src/main/java/com/dragontalker/MainTest.java
+++ spring-annotation/src/main/java/com/dragontalker/MainTest.java
@@ -1,8 +1,19 @@
 package com.dragontalker;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.dragontalker.bean.Person;
+
 public class MainTest {
 
 	public static void main(String[] args) {
 		
+		ClassPathXmlApplicationContext context = 
+				new ClassPathXmlApplicationContext("bean.xml");
+		
+		Person person = context.getBean("person", Person.class);
+		System.out.println(person);
+		
+		context.close();
 	}
 }
